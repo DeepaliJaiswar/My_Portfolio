76e6d1ca90763772cd42325c2c1326915dae63b7
Deepali Jaiswar <deepalijaiswar2003@gmail.com>
2024-04-25 15:32:24 +0530

    My_Protfolio

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ $(document).ready(function () {
     });
   
     var typed = new Typed(".typing", {
-      strings: ["BSc Computer Science Graduate", "Programmer", "Web Developer"],
+      strings: ["Graduated in BSc Computer Science", "Programmer", "Web Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["BSc Computer Science Graduate", "Programmer", "Web Developer"],
+      strings: ["Graduated in BSc Computer Science", "Programmer", "Web Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
